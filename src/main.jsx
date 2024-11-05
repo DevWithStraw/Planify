7daac61a810ad1bab779596fa165f04837d6d5eb
Mehdi <01skull01@gmail.com>
2024-11-06 01:54:02 +0330

    useQuery was missing the new and was wrapped up wrong

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import Home from "./pages/home";
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = QueryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
