45076d5514f7252ed83a1804c54adddcf6a3821a
Mehdi <01skull01@gmail.com>
2024-11-06 01:28:54 +0330

    installed and initilized the tanstack react query lib

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 
 import Home from "./pages/home";
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
+    </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
