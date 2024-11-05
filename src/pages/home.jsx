fb31fdec8554f16bc30b8d3aa7c957bb87abec97
Mehdi <01skull01@gmail.com>
2024-11-05 23:04:03 +0330

    changed the home page for executing project planify

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,37 +1,8 @@
-import { useState } from "react";
+import React from 'react'
 import "./home.scss";
 
-function Home() {
-  const [count, setCount] = useState(0);
-
+export default function home() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/assets/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src="/assets/react.svg"
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <div>Homepage</div>
+  )
 }
-
-export default Home;
