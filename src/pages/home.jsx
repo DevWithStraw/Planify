58c0096332274929d76ab7885141a1e0d4d7e695
Mehdi <01skull01@gmail.com>
2024-11-06 01:54:42 +0330

    defined a new query for the tasks

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 import "./home.scss";
 
-import Task from '../components/task';
+import Task from "../components/task";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import { baseUrl } from "../helpers/urls";
 
 export default function home() {
 
-  
+  const fetchTasks = async () => {
+    return await axios.get(`${baseUrl}/tasks`).then((res) => res.data)
+  };
+
+  const { data: tasks } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+  });
 
   return (
     <>
-    {tasks?.map((task)=>(
-      <Task title={task.title}/>
-    ))}
+      {tasks?.map((task) => (
+        <Task title={task.title} />
+      ))}
     </>
-  )
+  );
 }
