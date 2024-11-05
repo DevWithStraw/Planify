63aae9ed9c1f07f24040af8640614b1183ac4e47
Mehdi <01skull01@gmail.com>
2024-11-06 02:31:22 +0330

    created the logical tenary for tasks

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,9 +8,8 @@ import axios from "axios";
 import { baseUrl } from "../helpers/urls";
 
 export default function home() {
-
   const fetchTasks = async () => {
-    return await axios.get(`${baseUrl}/tasks`).then((res) => res.data)
+    return await axios.get(`${baseUrl}/tasks`).then((res) => res.data);
   };
 
   const { data: tasks } = useQuery({
@@ -20,9 +19,15 @@ export default function home() {
 
   return (
     <>
-      {tasks?.map((task) => (
-        <Task title={task.title} />
-      ))}
+      {tasks ? (
+        tasks?.map((task) => <Task title={task.title} />)
+      ) : (
+        <section className="empty-tasks-section">
+          <img src="/assets/icons/checked.svg" alt="checked icon" />
+          <h1> Add Some Tasks </h1>
+          <span> Press a to create a new task </span>
+        </section>
+      )}
     </>
   );
 }
