96db9d7d7db2fb54ebca025e6e341927d0d1ba01
Mehdi <01skull01@gmail.com>
2024-11-07 00:44:31 +0330

    created the add button component

---
+++ src/components/add-button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./add-button.scss";
+
+export default function AddButton() {
+  return (
+    <button type="button">
+      <img src="/assets/icons/plus.svg" alt="add a task" />
+    </button>
+  );
+}
