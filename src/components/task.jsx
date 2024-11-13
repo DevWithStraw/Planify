f2244124c565a4af61625aa48954744de1e8f968
Mehdi <01skull01@gmail.com>
2024-11-14 01:39:38 +0330

    updated the id for input and label

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import './task.scss';
+import React from "react";
+import "./task.scss";
+import axios from "axios";
+import { baseUrl } from "../helpers/urls";
 
-export default function task({title , id}) {
+export default function task({ title, id, checked }) {
+  const toggleTask = (event) => {
+    axios.patch(`${baseUrl}/tasks/${id}`, {
+      checked: event.target.checked,
+    });
+  };
+
   return (
-    <div className='task-container'>
-        <input type="checkbox" id={`${task}-${id}`} />
-        <label htmlFor="task"> {title} </label>
-        <div className="more-options">
-            {/* icon here */}
-        </div>
+    <div className="task-container">
+      <input
+        type="checkbox"
+        id={`task-${id}`}
+        defaultChecked={checked}
+        onClick={toggleTask}
+      />
+      <label htmlFor={`task-${id}`}> {title} </label>
+      <div className="more-options">{/* icon here */}</div>
     </div>
-  )
+  );
 }
